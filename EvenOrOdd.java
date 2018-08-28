96743aed5f0d71898e0c50cb4677679e53989345
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-28 00:42:54 -0400

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class EvenOrOdd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int k;
+		k=n-m;
+		if(k%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+}
