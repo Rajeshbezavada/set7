34e75293acb90fe6936f1b3620bd58bf2a95b2a5
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-28 00:24:38 -0400

    Create WithoutSemicolon.java

---
+++ WithoutSemicolon.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class WithoutSemicolon
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		if(System.out.printf(str)==null)
+		{
+			
+		}
+	}
+}
