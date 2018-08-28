dd8985d05842d2a1a5a245f1d924d627d749b4b4
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-28 00:51:20 -0400

    Create Program70.java

---
+++ Program70.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Program70
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int r;
+		r=n*2;
+		System.out.println(r);
+	}
+}
